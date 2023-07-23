a565149b79f0439689ab475a585fbf73feb8e3dc
barbaragargyan <barbara.gargyan@flowacademy.hu>
2023-07-23 15:52:18 +0200

    2.feladat

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("hello");
+        int count = 0;
+        // Ciklus, amely az első 10 hárommal osztható páros számot keresi
+        for (int i = 1; count < 10; i++) {
+            // Ha i páros és hárommal osztható, akkor kiírjuk
+            if (i % 2 == 0 && i % 3 == 0) {
+                System.out.print(i + " ");
+                count++;
+            }
+        }
     }
 
 }
